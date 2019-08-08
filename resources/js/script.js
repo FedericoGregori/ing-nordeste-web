060917053a2f9200823e1873005423d3b8cecfde
Federico Gregori <jfedericogregori@gmail.com>
2019-08-08 19:16:29 -0300

    [UPD] Styles, animations

---
+++ resources/js/script.js
@@ -0,0 +1,86 @@
+$(document).ready(function() {
+    /* $(".js--section-features").waypoint(
+        function(direction) {
+            if (direction == "down") {
+                $("header").addClass("sticky");
+            } else {
+                $("header").removeClass("sticky");
+            }
+        },
+        {
+            offset: "25%;"
+        }
+    ); */
+
+    /* Navigation scroll */
+    $(function() {
+        $("a[href*=\\#]:not([href=\\#])").click(function() {
+            if (
+                location.pathname.replace(/^\//, "") ==
+                    this.pathname.replace(/^\//, "") &&
+                location.hostname == this.hostname
+            ) {
+                var target = $(this.hash);
+                target = target.length
+                    ? target
+                    : $("[name=" + this.hash.slice(1) + "]");
+                if (target.length) {
+                    $("html,body").animate(
+                        {
+                            scrollTop: target.offset().top - 50
+                        },
+                        1000
+                    );
+                    return false;
+                }
+            }
+        });
+    });
+
+    /* Animations on scroll */
+
+    $(".js--wp-1").waypoint(
+        function(direction) {
+            $(".js--wp-1").addClass("animated slideInRight");
+        },
+        {
+            offset: "70%"
+        }
+    );
+
+    $(".js--wp-2").waypoint(
+        function(direction) {
+            $(".js--wp-2").addClass("animated fadeInUp");
+        },
+        {
+            offset: "70%"
+        }
+    );
+
+    $(".js--wp-3").waypoint(
+        function(direction) {
+            $(".js--wp-3").addClass("animated fadeInLeft");
+        },
+        {
+            offset: "70%"
+        }
+    );
+
+    $(".js--wp-4").waypoint(
+        function(direction) {
+            $(".js--wp-4").addClass("animated fadeInUp");
+        },
+        {
+            offset: "70%"
+        }
+    );
+
+    $(".js--wp-5").waypoint(
+        function(direction) {
+            $(".js--wp-5").addClass("animated fadeInLeft");
+        },
+        {
+            offset: "70%"
+        }
+    );
+});
